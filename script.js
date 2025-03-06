852583f5385ea76a7950b2e7de2622297cf014dc
JamesG <57632668+JimmyMcKegger@users.noreply.github.com>
2025-03-06 14:07:23 +0000

    updatae-date

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-var countDownDate = new Date("Dec 31, 2024 23:59:59").getTime();
+const countDownDate = new Date("Dec 31, 2025 23:59:59").getTime();
 
-var x = setInterval(function () {
-	var now = new Date().getTime();
+const x = setInterval(function () {
+	const now = new Date().getTime();
 
-	var distance = countDownDate - now;
+	const distance = countDownDate - now;
 
-	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+	const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
 	document.getElementById("days").innerHTML = days;
 	document.getElementById("hours").innerHTML = hours;
